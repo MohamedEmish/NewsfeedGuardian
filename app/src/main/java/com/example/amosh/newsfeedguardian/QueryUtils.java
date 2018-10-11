8ca5b9039f7e49da40f7f9fd0ace44466317fe7d
MohamedEmish <emish52020@gmail.com>
2018-10-11 15:11:59 +0200

    O.o

--- app/src/main/java/com/example/amosh/newsfeedguardian/QueryUtils.java
+++ app/src/main/java/com/example/amosh/newsfeedguardian/QueryUtils.java
@@ -1,17 +1,26 @@
 package com.example.amosh.newsfeedguardian;
 
+import android.icu.text.SimpleDateFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.Log;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class QueryUtils {
 
     private QueryUtils() {
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public static List<Feed> extractFeeds(String json) {
 
         List<Feed> feeds = new ArrayList<>();
@@ -19,21 +28,22 @@ public class QueryUtils {
         try {
             JSONObject jsonResponse = new JSONObject(json);
 
-            if (jsonResponse.getInt("totalItems") == 0) {
+
+            JSONObject response = jsonResponse.getJSONObject("response");
+            JSONArray results = response.getJSONArray("results");
+            if (results.length() == 0) {
                 return feeds;
             }
 
-            JSONArray jsonArray = jsonResponse.getJSONArray("response");
+            for (int i = 0; i < results.length(); i++) {
 
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject feedObject = jsonArray.getJSONObject(i);
+                JSONObject feedInfo = results.getJSONObject(i);
 
-
-                JSONObject feedInfo = feedObject.getJSONObject("results");
                 String title = feedInfo.getString("webTitle");
                 String url = feedInfo.getString("webUrl");
                 String section = feedInfo.getString("sectionName");
-                String date = feedInfo.getString("webPublicationDate");
+                String dated = feedInfo.getString("webPublicationDate");
+                String date = formatDate(dated);
                 Feed feed = new Feed(url, title, date, section);
 
                 feeds.add(feed);
@@ -44,6 +54,21 @@ public class QueryUtils {
         }
 
         return feeds;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private static String formatDate(String rawDate) {
+        String jsonDatePattern = "yyyy-MM-dd'T'HH:mm:ss'Z'";
+        SimpleDateFormat jsonFormatter = new SimpleDateFormat(jsonDatePattern, Locale.US);
+        try {
+            Date parsedJsonDate = jsonFormatter.parse(rawDate);
+            String finalDatePattern = "MMM d, yyy";
+            SimpleDateFormat finalDateFormatter = new SimpleDateFormat(finalDatePattern, Locale.US);
+            return finalDateFormatter.format(parsedJsonDate);
+        } catch (ParseException e) {
+            Log.e("QueryUtils", "Error parsing JSON date: ", e);
+            return "";
+        }
     }
 }
 
